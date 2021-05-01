2fe2f02d9bf5345ce3aae90a251172f3a62c3e99
xoneUpx <bobokuos@gmail.com>
2021-05-01 23:41:02 +0200

    test8

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { GetStaticProps } from 'next'
-import Post from '@components/Post.tsx'
+import Post from '@components/Post'
 
 export default function Home(props) {
   return (
